fc5a643cb2181ef9dc8ad21800dffeb5316c9aae
Ben Cheng <ben.c@spritely.co.nz>
2023-06-13 10:06:38 +1200

    feat: Create useFetch hook

---
+++ hook/useFetch.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: {
+      ...query,
+    },
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setLoading(false);
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    fetchData();
+  };
+
+  return { data, loading, error, refetch };
+};
+
+export default useFetch;
