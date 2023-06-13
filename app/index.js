4b1901f5c01db4cc748a961c6ddfca6ca20bd946
Ben Cheng <ben.c@spritely.co.nz>
2023-06-13 15:14:59 +1200

    feat: Create job-details route

--- app/index.js
+++ app/index.js
@@ -1,19 +1,46 @@
 import { useState } from "react";
 import { View, ScrollView, SafeAreaView, Text } from "react-native";
-import { Stack, Route, useRouter } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 
 import { COLORS, ICONS, IMAGES, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
   const router = useRouter();
+  const [searchWord, setSearchWord] = useState("");
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
         options={{
           headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => <ScreenHeaderBtn iconUrl={ICONS.menu} size="60%" />,
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={IMAGES.profile} size="100%" />
+          ),
+          headerTitle: "",
         }}
       />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome
+            searchTerm={searchWord}
+            setSearchTerm={setSearchWord}
+            handleClick={() => {
+              if (searchWord) router.push(`/search/${searchWord}`);
+            }}
+          />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
