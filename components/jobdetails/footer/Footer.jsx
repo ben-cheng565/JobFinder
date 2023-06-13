395a9a7c52bc87d646cce0ef90c83b734a390459
Ben Cheng <ben.c@spritely.co.nz>
2023-06-13 15:14:16 +1200

    feat: Create footer component

---
+++ components/jobdetails/footer/Footer.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
+
+import styles from "./footer.style";
+import { ICONS } from "../../../constants";
+
+const Footer = ({ url }) => {
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.likeBtn} onPress={() => setLiked(!liked)}>
+        <Image
+          source={liked ? ICONS.heart : ICONS.heartOutline}
+          resizeMode="contain"
+          style={styles.likeBtnImage}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.applyBtn}
+        onPress={() => Linking.openURL(url)}
+      >
+        <Text style={styles.applyBtnText}>Apply for job</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Footer;
